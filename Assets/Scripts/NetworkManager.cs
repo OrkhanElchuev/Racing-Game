7cd3aaefa19b37aa59674b1444927ef31048b47f
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-12-18 10:42:23 +0100

    Login button implemented

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [Header("Login UI")]
+    public InputField playerNameInput;
+
+
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #endregion
+
+    #region UI Callback methods
+
+    public void OnLoginButtonClicked()
+    {
+        string playerName = playerNameInput.text;
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            if (!PhotonNetwork.IsConnected)
+            {
+                // Set the player name in network and connect 
+                PhotonNetwork.LocalPlayer.NickName = playerName;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+        else
+        {
+            Debug.Log("Player name is Invalid");
+        }
+    }
+
+    #endregion
+}

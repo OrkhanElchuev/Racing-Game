6faa61bcd06e1ba5f0c7adaf191e0d2ae2094b39
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-12-23 20:35:43 +0300

    Lobby panels assigned, activating relevant panel added

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -7,21 +7,55 @@ using Photon.Pun;
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
     [Header("Login UI")]
+    public GameObject loginUIPanel;
     public InputField playerNameInput;
 
+    [Header("Connecting Info Panel")]
+    public GameObject connectingInfoUIPanel;
 
+    [Header("Creating Room Info Panel")]
+    public GameObject creatingRoomInfoUIPanel;
+
+    [Header("GameOptions  Panel")]
+    public GameObject gameOptionsUIPanel;
+
+    [Header("Create Room Panel")]
+    public GameObject createRoomUIPanel;
+
+    [Header("Inside Room Panel")]
+    public GameObject insideRoomUIPanel;
+   
+    [Header("Join Random Room Panel")]
+    public GameObject joinRandomRoomUIPanel;
+
+    
     #region Unity Methods
 
     // Start is called before the first frame update
     void Start()
     {
-
+         
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    #endregion
+    
+    #region Public Methods
+
+    // Activate relevant panel
+    public void ActivatePanel(string panelNameToBeActivated)
+    {
+        loginUIPanel.SetActive(loginUIPanel.name.Equals(panelNameToBeActivated));
+        connectingInfoUIPanel.SetActive(connectingInfoUIPanel.name.Equals(panelNameToBeActivated));
+        creatingRoomInfoUIPanel.SetActive(creatingRoomInfoUIPanel.name.Equals(panelNameToBeActivated));
+        createRoomUIPanel.SetActive(createRoomUIPanel.name.Equals(panelNameToBeActivated));
+        gameOptionsUIPanel.SetActive(gameOptionsUIPanel.name.Equals(panelNameToBeActivated));
+        joinRandomRoomUIPanel.SetActive(joinRandomRoomUIPanel.name.Equals(panelNameToBeActivated));
     }
 
     #endregion
@@ -45,6 +79,23 @@ public class NetworkManager : MonoBehaviourPunCallbacks
             Debug.Log("Player name is Invalid");
         }
     }
+
+    #endregion
+
+    #region Photon Callbacks
+
+    // On connected to the Internet
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to the Internet");
+    }
+
+    // On connected to the Photon Server
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to the server");
+    }
+
 
     #endregion
 }

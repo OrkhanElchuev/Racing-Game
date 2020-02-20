21bb090331ad6c4bd8c7bb8e1949839556bd8e06
OrkhanElchuev <orkhan.elchuev@gmail.com>
2020-02-20 20:38:45 +0100

    Shooting mechanism implemented

---
+++ Assets/Scripts/Death_Race_Mode/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Initializing bullet 
+    public void Initialize (Vector3 direction, float speed, float damage)
+    {
+        transform.forward = direction;
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.velocity = direction * speed;
+    }
+}
---
+++ Assets/Scripts/Death_Race_Mode/Shooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePosition;
+    public Camera playerCamera;
+    public DeathRacePlayer deathRacePlayerProperties;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        // If "space" key is pressed then shoot
+        if (Input.GetKey ("space"))
+        {
+            Fire ();
+        }
+    }
+
+    // Method for instantiating bullets
+    public void Fire ()
+    {
+        // 0.5f, 0.5f for allocating ray in the middle of the screen
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        GameObject bulletGameObject = Instantiate(bulletPrefab, firePosition.position, Quaternion.identity);
+        bulletGameObject.GetComponent<Bullet>().Initialize(ray.direction, deathRacePlayerProperties.bulletSpeed, deathRacePlayerProperties.damage);
+    }
+}

f8c8f419f49c2a3210afc977d63a1b54bc1e8b1a
OrkhanElchuev <orkhan.elchuev@gmail.com>
2020-02-20 01:16:15 +0100

    Avoid controlling other players camera and car bug

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    public Camera playerCamera;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        // Avoid controlling other players in the room, make sure to control only your own player
+        if (photonView.IsMine)
+        {
+            GetComponent<CarMovement> ().enabled = true;
+            playerCamera.enabled = true;
+        }
+        else
+        {
+            GetComponent<CarMovement> ().enabled = false;
+            playerCamera.enabled = false;
+        }
+    }
+}

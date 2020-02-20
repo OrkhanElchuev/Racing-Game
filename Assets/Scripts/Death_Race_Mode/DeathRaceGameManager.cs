c73d9bd6da4420c097a6aef1407e61204656203b
OrkhanElchuev <orkhan.elchuev@gmail.com>
2020-02-20 23:05:00 +0100

    Quit match button added into death race mode

--- Assets/Scripts/Death_Race_Mode/DeathRaceGameManager.cs
+++ Assets/Scripts/Death_Race_Mode/DeathRaceGameManager.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Death race mode game manager
-public class DeathRaceGameManager : MonoBehaviour
+public class DeathRaceGameManager : MonoBehaviourPunCallbacks
 {
     public GameObject[] carPrefabs;
 
@@ -17,9 +18,22 @@ public class DeathRaceGameManager : MonoBehaviour
             if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue (MultiplayerRacingGame.CAR_SELECTION_NUMBER, out carSelectionNumber))
             {
                 // Instantiating players on random position in a range of coordinates(-16, 16)
-                int randomPosition = Random.Range(-16, 16);
-                PhotonNetwork.Instantiate(carPrefabs[(int)carSelectionNumber].name, new Vector3(randomPosition, 0, randomPosition), Quaternion.identity);
+                int randomPosition = Random.Range (-16, 16);
+                PhotonNetwork.Instantiate (carPrefabs[(int) carSelectionNumber].name, new Vector3 (randomPosition, 0, randomPosition), Quaternion.identity);
             }
         }
+    }
+
+    // Quit Match button is located inside Death Match Scene
+    public void OnQuitMatchButtonClicked ()
+    {
+        // Leave the room
+        PhotonNetwork.LeaveRoom ();
+    }
+
+    // Load Lobby scene when player leaves the room
+    public override void OnLeftRoom ()
+    {
+        SceneManager.LoadScene ("LobbyScene");
     }
 }

13f497c481b716e5d649ed4914e2e2c7fe4101d9
OrkhanElchuev <orkhan.elchuev@gmail.com>
2020-02-20 01:29:26 +0100

    Singleton pattern implemented

--- Assets/Scripts/RacingModeGameManager.cs
+++ Assets/Scripts/RacingModeGameManager.cs
@@ -8,6 +8,25 @@ public class RacingModeGameManager : MonoBehaviour
     public GameObject[] carPrefabs;
     public Transform[] instantiatePositions;
 
+    // Singleton implementation 
+    public static RacingModeGameManager instance = null;
+    private void Awake ()
+    {
+        // Check if instance already exists
+        if (instance == null)
+        {
+            instance = this;
+        }
+        // If instance already exists and it is not this
+        else if (instance != this)
+        {
+            // Destroy this. This enforces our singleton pattern, we can have only one singleton
+            Destroy(gameObject);
+        }
+        // To not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start ()
     {
@@ -20,7 +39,7 @@ public class RacingModeGameManager : MonoBehaviour
                 int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
                 // Instantiating players on different spots 
                 Vector3 instantiatePos = instantiatePositions[actorNumber - 1].position;
-                PhotonNetwork.Instantiate(carPrefabs[(int)carSelectionNumber].name, instantiatePos, Quaternion.identity);
+                PhotonNetwork.Instantiate (carPrefabs[(int) carSelectionNumber].name, instantiatePos, Quaternion.identity);
             }
         }
     }
---
+++ Assets/Scripts/TimeCountDownManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class TimeCountDownManager : MonoBehaviourPunCallbacks
+{
+    private Text timeUItext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

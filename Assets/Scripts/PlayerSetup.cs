7bbf84b6d7e167db04004f7bac98d6d82bb23325
OrkhanElchuev <orkhan.elchuev@gmail.com>
2020-02-20 15:59:12 +0100

    Displaying nickname above player car

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -1,29 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
 
 // Player initial setup
 public class PlayerSetup : MonoBehaviourPunCallbacks
 {
     public Camera playerCamera;
+    public TextMeshProUGUI playerNameText;
 
     // Start is called before the first frame update
     void Start ()
     {
-        FindObjectOfType<Camera>().GetComponent<AudioListener>().enabled = false; 
+        // Avoid multiple camera audio listeners in one room issue
+        FindObjectOfType<Camera> ().GetComponent<AudioListener> ().enabled = false;
+
         // Avoid controlling other players in the room, make sure to control only your own player
         if (photonView.IsMine)
         {
             GetComponent<CarMovement> ().enabled = true;
-            GetComponent<LapController>().enabled = true;
+            GetComponent<LapController> ().enabled = true;
             playerCamera.enabled = true;
         }
         else
         {
             GetComponent<CarMovement> ().enabled = false;
-            GetComponent<LapController>().enabled = false;
+            GetComponent<LapController> ().enabled = false;
             playerCamera.enabled = false;
+        }
+        SetPlayerUI ();
+    }
+
+    // Setting player nick name
+    private void SetPlayerUI ()
+    {
+        // Avoid null reference exception
+        if (playerNameText != null)
+        {
+            playerNameText.text = photonView.Owner.NickName;
+            // Disable nickname field if player is myself
+            if (photonView.IsMine)
+            {
+                playerNameText.gameObject.SetActive (false);
+            }
         }
     }
 }
